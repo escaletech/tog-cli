dd27822f8e60c21c13f5aec15190b4ecd453ccd3
Rodrigo Dumont <rlsdumont@gmail.com>
2019-06-27 12:29:34 -0300

    Implement flags commands

--- index.js
+++ index.js
@@ -12,6 +12,13 @@ const argv = yargs
       yargs.positional('key', { description: 'configuration key to get/set' })
       yargs.positional('value', { implies: 'key', description: 'value to set' })
     }, require('./commands/config'))
+  .command('flags', 'read or update flags',
+    yargs => {
+      yargs.usage('tog flags [name [state]]')
+      yargs.positional('name', { description: 'flag name' })
+      yargs.positional('state', { description: 'flag state', choices: ['on', 'off'], implies: 'name' })
+      yargs.option('namespace', { alias: 'n', required: true })
+    }, require('./commands/flags'))
   .argv
 
 if (!argv) {
