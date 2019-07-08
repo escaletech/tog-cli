ecb97fce3419f1e8704215bbed6077e9193225d6
Rodrigo Dumont <rlsdumont@gmail.com>
2019-07-08 19:23:54 -0300

    Add some aliases to commands

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ const argv = yargs
     yargs =>
       yargs.describe('host', 'tog server host').alias('h', 'host')
     , require('./commands/login'))
-  .command('config', 'list configuration',
+  .command(['config', 'conf'], 'list or update configuration',
     yargs => {
       yargs.usage('tog config [key [value]]')
       yargs.positional('key', { description: 'configuration key to get/set' })
       yargs.positional('value', { implies: 'key', description: 'value to set' })
     }, require('./commands/config'))
-  .command('flags', 'read or update flags',
+  .command(['flag', 'flags'], 'read or update flags',
     yargs => {
       yargs.usage('tog flags [name [state]]')
       yargs.positional('name', { description: 'flag name' })
       yargs.positional('state', { description: 'flag state', choices: ['on', 'off'], implies: 'name' })
       yargs.option('namespace', { alias: 'n', required: true })
     }, require('./commands/flags'))
-  .command('exp', 'read or update experiments',
+  .command(['exp', 'experiment', 'experiments'], 'read or update experiments',
     yargs => {
       yargs.usage('tog exp [name]')
       yargs.positional('name', { description: 'experiment name' })
